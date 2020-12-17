1c50565373afe40cddfa83a06b6b10b7dd8d43c2
maximus7022 <pash6790@gmail.com>
2020-12-17 23:25:25 +0200

    Final change

--- JS/script.js
+++ JS/script.js
@@ -85,30 +85,3 @@ $('.decor').submit(function validate_form() {
 	}
 
 });
-
-function select_all() {
-	$.post(
-        './PHP/select_all.php',    
-        function(msg) {
-            $('#message_1').html(msg);
-        }
-    );
-}
-
-function select_ten() {
-	$.post(
-        './PHP/select_ten.php',    
-        function(msg) {
-           	$('#message_1').html(msg);
-        }
-    );
-}
-
-function select_count() {
-	$.post(
-        './PHP/select_count.php',    
-        function(msg) {
-           	$('#message_1').html(msg);
-        }
-    );
-}
--- PHP/select_ten.php
+++ PHP/select_ten.php
@@ -1,23 +1,19 @@
 <?php
-	$mysql = new mysqli('127.0.0.1', 'maximus7022', '12345Max', 'maximus7022');
-	$query ="SELECT * FROM Registration WHERE ID <= 10";
-    $result = mysqli_query($mysql, $query) or die("Виникла помилка: " . mysqli_error($mysql)); 
-	if($result)
-	{
-    	$rows = mysqli_num_rows($result);
-     
-	    echo "<table><tr><th>Id</th><th>PIB</th><th>Groupa</th><th>Birth</th><th>Age</th><th>Specialty</th><th>Email</th><th>AverageMark</th></tr>";
-	    for ($i = 0 ; $i < $rows ; ++$i)
-    	{
-        	$row = mysqli_fetch_row($result);
-        	echo "<tr>";
-            	for ($j = 0 ; $j < 8 ; ++$j) echo "<td>$row[$j]</td>";
-        	echo "</tr>";
-    	}
-    	echo "</table>";
-     
-    mysqli_free_result($result);
-	}
- 
-	mysqli_close($mysql);
+    $mysql = new mysqli('127.0.0.1', 'maximus7022', '12345Max', 'maximus7022');
+
+    if ($mysql->connect_error)
+    {
+        die("Не вдалося підключитись: " .$mysql->connect_error);
+    }
+
+    $mysql->set_charset(UTF8);
+
+    $result = $mysql->query("SELECT * FROM Registration WHERE ID <= 10");
+
+    echo "<table><tr><th>Id</th><th>PIB</th><th>Groupa</th><th>Birth</th><th>Age</th><th>Specialty</th><th>Email</th><th>AverageMark</th></tr>";
+
+    while($row = $result->fetch_assoc())
+    {
+        echo "<tr><td>".$row['ID']."</td><td>".$row['PIB']."</td><td>".$row['Groupa']."</td><td>".$row['Birth']."</td><td>".$row['Age']."</td><td>".$row['Specialty']."</td><td>".$row['Email']."</td><td>".$row['AverageMark']."</td></tr>";
+    }
 ?>
